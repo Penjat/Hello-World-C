afcb670760dcb47456da6948dfd9711ba7961872
SpencerSymington <theSpencerSymington@gmail.com>
2018-10-28 20:24:40 -0230

    Initial Commit

---
+++ hello-world/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hello-world
+//
+//  Created by Spencer Symington on 2018-10-28.
+//  Copyright © 2018 Spencer Symington. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
